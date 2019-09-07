bd11981d5846062b658a82c85f61cf164b4a5ad5
warrenm16 <warrenm16@cs104u-6d.acad.fullerton.edu>
2019-09-07 13:13:35 -0700

    diamond complete

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -1,10 +1,18 @@
-// Name: 
+// Name:
 // This program displays a diamond pattern to the screen.
 
 #include <iostream>
 
 int main()
 {
-
+  // Print a diamond
+  std::cout << "    *    \n";
+  std::cout << "   ***   \n";
+  std::cout << "  *****  \n";
+  std::cout << " ******* \n";
+  std::cout << "  *****  \n";
+  std::cout << "   ***   \n";
+  std::cout << "    *    \n";
+    return 0;
   return 0;
 }
